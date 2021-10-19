3d6d763abd51e82b510709bf40bc9c310d5eec34
Ayriel Noriega <anoriega2310@gmail.com>
2021-10-18 23:02:22 -0500

    Creacion layout and conexion pdo

---
+++ includes/db.php
@@ -0,0 +1,32 @@
+<?php
+
+class DB
+{
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    private $charset;
+
+    public function __construct()
+    {
+        $this->host = 'localhost';
+        $this->db = 'encuestas';
+        $this->user = 'root';
+        $this->password = '';
+        $this->charset = 'utf8mb4';
+    }
+
+    public function connect()
+    {
+        try{
+            $connection = "mysql:host=" . $this->host . ";dbname=" . $this->db . ";charset=" . $this->charset; 
+
+            $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                       PDO::ATTR:EMULATE:PREPARES => FALSE];
+            $pdo = new PDO($connection, $this->user, $this->password, $options);
+        }catch( PDOException $e ){
+            print_r('Error en la conexión ' . $e->getMessage());
+        }
+    }
+}
